e1f521c0d8ac60a55d1d6a06c741e4f99f5ecc86
Thomas Kelly <tmkelly28@gmail.com>
2017-01-31 13:25:51 -0500

    a little bit more cleanup

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('*', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  console.log(socket.id, ' connected');
+  console.log(socket.id, 'connected');
 
   socket.emit('load', inMemoryDrawHistory);
 
